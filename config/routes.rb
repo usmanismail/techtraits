2bb85e7ddf13ceefbcb0e49fefaed57f2478c7ed
Usman Ismail <0xfffffff@gmail.com>
2011-03-03 12:49:30 -0500

    Adding basic article and comment cruds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -29,15 +33,8 @@ ActionController::Routing::Routes.draw do |map|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
+  map.resources :articles, :has_many => :comments
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => ":article"
 end
